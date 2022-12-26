cd72b6f228b130684ad9dc6b93b49e7e0141d295
~iz <43661631+mencretsu@users.noreply.github.com>
2022-12-26 17:43:58 +0700

    Update mypytesseract.py

--- mypytesseract.py
+++ mypytesseract.py
@@ -8,7 +8,8 @@ pytesseract.pytesseract.tesseract_cmd=r'C:\Program Files\Tesseract-OCR\tesseract
 for filename in os.listdir(folder_path):
     file_path = os.path.join(folder_path, filename)
     text = pytesseract.image_to_string(file_path)
-    if "'highlight'>" in text:
+    # replace 'sasu'
+    if "'sasu'>" in text:
         os.remove(file_path)
         found += 1
         print(f"{found} Found !")
